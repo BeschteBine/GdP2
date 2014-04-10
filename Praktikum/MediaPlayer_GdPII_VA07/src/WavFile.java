fd625c7aed31d8565c864b8321683f6af537eb1d
Lars Tragl <lars@vmmint13-ss14.(none)>
2014-04-10 16:58:26 +0200

    added MediaPlayer_VA07 (working draft)

---
+++ Praktikum/MediaPlayer_GdPII_VA07/src/WavFile.java
@@ -0,0 +1,20 @@
+
+public class WavFile {
+
+//liefert die zur Audiodatei "Dateiname" gehörenden Daten
+	WavParamReader.readParams();{
+		
+	}
+
+//Anzahl der Frames pro Sekunde	
+	static WavParamReader.getFrameRate();{
+		
+	}
+//Gesamtanzahl der Frames
+	static WavParamReader.getNumberOfFrames();{
+		
+	}
+	
+	public static long
+	
+}
