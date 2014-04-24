cde9f0d31774c1680646e65172905e19c46f0cdc
Lars Tragl <lars@vmmint13-ss14.(none)>
2014-04-24 10:54:48 +0200

    finished Va08

---
+++ Praktikum/MediaPlayer_GdPII_VA08/src/AudioFileFactory.java
@@ -0,0 +1,23 @@
+public class AudioFileFactory {
+
+	public static AudioFile getInstance(String pathname) {
+		// input check
+		if (pathname == null) throw new RuntimeException("pathname can not be null");
+		if (pathname.isEmpty() || !pathname.contains(".")) {
+			throw new RuntimeException("Unknow suffix for AudioFile: \""
+					+ pathname + "\"");
+		}
+		
+		// do the magic
+		AudioFile af = null;
+		if (pathname.endsWith(".wav")) {
+			WavFile wf = new WavFile(pathname);
+			af = (AudioFile) wf;
+		} else if (pathname.endsWith(".mp3") || pathname.toLowerCase().endsWith(".ogg")) {
+			TaggedFile tf = new TaggedFile(pathname);
+			af = (AudioFile) tf;
+		}
+		return af;
+	}
+}// end
+
